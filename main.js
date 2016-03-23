d182c9617b95fea9227af4c5c5cce69b6339dcda
Xhya <alexia.souvane@gmail.com>
2016-03-23 15:52:02 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,35 +1,92 @@
 /*
-* Title : Annotation dynamic treemap
+* Title : Annotation dynamic treemap 
 * Author : Alexia
 * Date : 03/19/2016
 */
 
 
-// Used tutorial from https://codeforgeek.com/2015/01/render-html-file-expressjs/
+// Used tutorial from https://codeforgeek.com/2015/01/render-html-file-expressjs/ to insert file.html
+// Used tutorial from https://codeforgeek.com/2014/11/file-uploads-using-node-js/ to upload user file
+// Used tutorial from https://www.terlici.com/2015/05/16/uploading-files-locally.html
 
-/*
-ExpressJS allows you to develop custom web server according to your need. 
-You don’t need to install multiple packages to handle HTML files. 
-*/
-// Insert modules "express" and "path"
-var express = require("express");
-var path    = require("path");
+// Insert required modules 
+var express = require("express");   // to create web server
+var path    = require("path");      // to send html file to front-end
+var multer  = require('multer');    // to upload file
+var fs      = require("fs");        // to read/write in file
+var script  = require('./ON_SERVER/scripts/scripts'); // Our own module with our scripts
 
 
 // Instanciate an object express named "app"
 var app = express(); 
 
 
+/*
+We have made custom middle-ware function to choose the storage engine 
+which is Disk because we want files to store in disk 
+and appending the file name with current date just to keep the uniqueness of files.
+*/
 
-// Insert html page 
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname+'/index.html'));
-  //__dirname : It will resolve to your project folder.
+var storage =   multer.diskStorage({
+  // Put input file into uploads folder
+  destination: function (req, file, callback) {
+    callback(null, './ON_SERVER/uploads');
+  },
+  // Get file name and date of input file 
+  filename: function (req, file, callback) {
+    callback(null, file.fieldname + '-' + Date.now());
+  }
 });
+var upload = multer({ storage : storage}).single('file_name');
+
+/* Structure we got after upload
+
+{ fieldname: 'file_name',
+  originalname: 'clusters',
+  encoding: '7bit',
+  mimetype: 'application/octet-stream',
+  destination: './uploads',
+  filename: 'file_name-1458738794393',
+  path: 'uploads/file_name-1458738794393',
+  size: 1075 }
+
+We can access all these data through "req.files.size" for example
+
+*/
+
+
+// Root page
+app.use(express.static(__dirname+"/index"));
+
+
+
+// After submission, file is uploaded (return an error if file not well uploaded)
+// and execute file conversion. Then launch treemap visualisation from the converted file
+app.post('/treemap_vis',function(req,res){
+    upload(req,res,function(err) {
+        if(err) {
+            return res.end("Error uploading file.");
+        }
+
+        // Convert csv file into JS objet
+        var object = script.objecting_csv(req.file.path);
+
+
+        //script.write(JSstr);
+        
+        res.end("Good job!");
+
+        // Tranform received file into JSON mixed with MongoDataBase data
+        //var content = fs.readFileSync("test.js", "UTF-8");
+    });
+});
+
+
+//var content = fs.readFileSync("ON_SERVER/scripts/convert_upload_to_json.js", "UTF-8");
+//vm.runInThisContext(content);
 
 
 // Create server
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+  console.log('listening on port 3000!');
 });
-
