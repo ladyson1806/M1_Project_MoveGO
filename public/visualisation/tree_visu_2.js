b82184440c65d461dfdf328218fa7a22044ba42a
KristinaKa <kristinkastano@gmail.com>
2016-05-11 16:58:31 +0200

    Add files via upload

--- public/visualisation/tree_visu_2.js
+++ public/visualisation/tree_visu_2.js
@@ -3,12 +3,12 @@
 * Original source : 
 * http://www.labri.fr/perso/aperrot/fatum/treedemo/index.html
 * Original author : David Auber
-* Modifications by : Kristina Kastano
+* Modifications by : Savandara Besse, Kristina Kastano
 * Last version : 08/04/2016
 *********************************************************/
 
 var labelList= new Array;
-var T_SIZE = 0.28; //taille du texte des labels
+var T_SIZE = 0.28; //text size for the labels
 
 var tmap = function (elem, depth, x0, y0, x1, y1, s, m) {
     if (elem.markt == undefined) {
@@ -21,8 +21,7 @@ var tmap = function (elem, depth, x0, y0, x1, y1, s, m) {
 	elem.markt.borderWidth(1);
     }
     if (m != undefined && elem.name == 'data'){
-	//détection du terme choisi
-	findChosenTerm(elem, m);
+	findChosenTerm(elem, m); 
 	document.getElementById("info_array").innerHTML = "";
     }
     
@@ -52,7 +51,6 @@ var tmap = function (elem, depth, x0, y0, x1, y1, s, m) {
     else
 	dalpha = (x1 - x0) / elem.size;
 
-    //Presence d'enfants, appel recursif à la fonction
     if (elem.children != undefined){
 	for (var i in elem.children) {
 	    if (s)
@@ -67,74 +65,87 @@ var tmap = function (elem, depth, x0, y0, x1, y1, s, m) {
 		x0 = x1;
 	}
     }
-    //Pas d'enfants, affichage du label et coloration enfants/parents
+
     else {
 	createLabel(elem, m, y0, y1, w, h);
 	colorRelatives(elem, m);
     }
 }
 
-function findChosenTerm(elem, m){
-    /* Ajoute comme attribut de m le code du terme choisi */
-    /* Si on a cliqué sur un cluster ou data, il ajoute rien */
-    if (elem.children != undefined && m.id() != elem.markt.id())
+function findChosenTerm(elem, chosenMark){
+    // Add of the GO term as attribute to the chosenMark
+    if (elem.children != undefined && chosenMark.id() != elem.markt.id())
 	for (var i in elem.children){
-	    findChosenTerm(elem.children[i], m);
+	    findChosenTerm(elem.children[i], chosenMark);
 	}
     else
-	if ( elem.markt.id() == m.id() ){
-	    m.term = elem.term; 
+	if ( elem.markt.id() == chosenMark.id() ){
+	    chosenMark.term = elem.term; 
 	}
 }
 
-function colorRelatives(elem, m){
-    //Colore le terme elem si il est fils ou père du terme représenté par le mark choisi m
-    if (m != undefined && m.term != undefined){
-	//m.chosenElem est undefined quand on a cliqué sur un cluster ou data
+function colorRelatives(elem, chosenMark){
+    //Colors the data element elem if child or parent of the chosenMark
+    if (chosenMark != undefined && chosenMark.term != undefined){
+	//chosenMark.term is undefined if the click was on a cluster or data
 	for (var i in elem.term_children){
-	    if ( elem.term_children[i] == m.term ){
-		elem.markt.color(51, 153, 255); //bleue, parent
+	    if ( elem.term_children[i] == chosenMark.term ){
+		elem.markt.color(51, 153, 255); //parent
 	    }
 	}
 	for (var j in elem.parents){
-	    if ( elem.parents[j] == m.term ){ 
-		elem.markt.color(255, 51, 51, 255); //rouge, enfant
+	    if ( elem.parents[j] == chosenMark.term ){ 
+		elem.markt.color(255, 51, 51, 255); //child
 	    }
 	}
     }
 }
 
 function createLabel(elem, m, y0, y1, w, h){
-    //creation d'un label (texte) pour chaque terme
+    //creation of a label for the element elem
     var name = elem.name;
     var label;
-    var r = 0; //rotation du label, par defaut la disposition est horizontale
+    var r = 0; //label rotation
     var rectCenterX = elem.markt.x()
     var rectCenterY = elem.markt.y();	
     
-    //si un rectangle a été cliqué
+    //label creation if elem is the chosen mark
     if ((m != undefined ) && ( elem.markt.id() == m.id())){
-	showInfoChosenElement(elem);
+    	if ((m != undefined ) && ( elem.markt.id() == m.id())){
+	    if (w >= (name.length*T_SIZE/2)){
+		label = window.fatum.addText().textColor(0,0,200,200).size(T_SIZE)
+		    .text(name).x(rectCenterX).y(rectCenterY).rotation(r); 
+		labelList.push(label);
+	    }
+	    else {
+		var tmp = name.split(" ");
+		for (var i=0; i<tmp.length; i++){
+		    var word = tmp[i];
+		    label = window.fatum.addText().textColor(0,0,200,200).size(T_SIZE)
+			.text(word).x(rectCenterX).y(y1-0.5-i*T_SIZE).rotation(r);
+		    labelList.push(label);
+		}
+	    }
+	    
+	    showInfoChosenElement(elem);
+	}
     }
     
-    //Affichage du nom de chaque terme
-    //Si la chaine entière peut entrer horizontalement ou verticalement dans le rectangle
-    if ((w >= (name.length*T_SIZE/2) ||  h >= (name.length*T_SIZE/2)) && (w>T_SIZE && h>T_SIZE) ){
-	//Si la chaine peut entrer verticalement seulement
+ 
+    //label creation if elem is any other term
+    else if ((w >= (name.length*T_SIZE/2) ||  h >= (name.length*T_SIZE/2)) && (w>T_SIZE && h>T_SIZE) ){
 	if (w < (name.length*T_SIZE/2) && h >= (name.length*T_SIZE/2))
-	    r = 1.57; //rotation de 90° (en radians)
+	    r = 1.57; //rotation of 90°
 		
 	label = window.fatum.addText().textColor(0,0,200,200).size(T_SIZE).text(name)
 	    .x(rectCenterX).y(rectCenterY).rotation(r); 
 	labelList.push(label);
-    }	
-    
-    //Autrement : separation du nom en plusieurs mots
+    }
+
     else {
 	var tmp = name.split(" ");
 	for (var i=0; i<tmp.length; i++)
 	    if (tmp[i].length*T_SIZE/2 > w || tmp[i].length*T_SIZE > h)
-		//Si la chaine separée n'entre pas, on ne mets pas le label
 		return;
 	for (var i=0; i<tmp.length; i++){
 	    var word = tmp[i];
@@ -142,8 +153,7 @@ function createLabel(elem, m, y0, y1, w, h){
 	    label = window.fatum.addText().textColor(0,0,200,200).size(T_SIZE).text(word).
 		x(rectCenterX).y(y1-0.5-i*T_SIZE).rotation(r);
 	    labelList.push(label);
-	}
-	
+	}	
     }
 }
 
@@ -154,81 +164,67 @@ function clearLabels(){
 
 function updateMarks(m) {
     clearLabels();
-    //elem, depth, x0, y0, x1, y1, s
     tmap(data, 1, - 20, -10, 13, 8, false, m);
 }
 
-
 function showInfoChosenElement(elem) {
-	var element = document.getElementById("info_array");
-	var html = "<br><br>";
-	var table_header = "";
-	table_header += '<table align="center">';
-	table_header += '<tr>';
-	table_header += '<th>Term Name</th>';
-	table_header += '<th>GO term</th>';
-	table_header += '<th>ICnuno</th>';
-	table_header += '<th>ICzhou</th>';
-	table_header += '<th>Genes</th>';
-	table_header += '<th>Parent terms</th>';
-	table_header += '<th>Child terms</th>';
-	table_header += '</tr>';
-	
-	
-	if (elem.children != undefined) {
-		html += '<h3 align="center">'+elem.name+'</h3>';
-		html += table_header;
-		for ( var i in elem.children) {		
-    		html += '<tr>';
-			html += '<td>'+elem.children[i].name+'</td>';
-			html += '<td>'+elem.children[i].term+'</td>';
-			html += '<td>'+Math.round(elem.children[i].ICnuno*100)/100+'</td>';
-			html += '<td>'+Math.round(elem.children[i].ICzhou*100)/100+'</td>';
-			html += '<td>';
-			for (var j in elem.children[i].gene) {
-				html += elem.children[i].gene[j];
-				if (j != elem.children[i].gene.length - 1)
-					html += ", ";
-			}
-			html += '</td>';
-			html += '<td>'+elem.children[i].parents+'</td>'
-			html += '<td>'+elem.children[i].term_children+'</td>'
-			html += '</tr>';
-    	}
-    }
-	
-	else {	
-	    html += table_header;
-	    html += '<tr>';
-	    html += '<td>'+elem.name+'</td>';
-	    html += '<td>'+elem.term+'</td>';
-	    html += '<td>'+Math.round(elem.ICnuno*100)/100+'</td>';
-	    html += '<td>'+Math.round(elem.ICzhou*100)/100+'</td>';
+    var element = document.getElementById("info_array");
+    var html = "<br><br>";
+    var table_header = "";
+    table_header += '<table align="center">';
+    table_header += '<tr>';
+    table_header += '<th>Term Name</th>';
+    table_header += '<th>GO term</th>';
+    table_header += '<th>ICnuno</th>';
+    table_header += '<th>ICzhou</th>';
+    table_header += '<th>Genes</th>';
+    table_header += '</tr>';
+    
+    if (elem.children != undefined) {
+	html += '<h3 align="center">'+elem.name+'</h3>';
+	html += table_header;
+	for ( var i in elem.children) {		
+    	    html += '<tr>';
+	    html += '<td>'+elem.children[i].name+'</td>';
+	    html += '<td>'+elem.children[i].term+'</td>';
+	    html += '<td>'+Math.round(elem.children[i].ICnuno*100)/100+'</td>';
+	    html += '<td>'+Math.round(elem.children[i].ICzhou*100)/100+'</td>';
 	    html += '<td>';
-	    for (var i in elem.gene) {
-		html += elem.gene[i];
-		if (i != elem.gene.length - 1)
+	    for (var j in elem.children[i].gene) {
+		html += elem.children[i].gene[j];
+		if (j != elem.children[i].gene.length - 1)
 		    html += ", ";
 	    }
 	    html += '</td>';
-	    html += '<td>'+elem.parents+'</td>';
-	    html += '<td>'+elem.term_children+'</td>';
 	    html += '</tr>';
+    	}
+    }  else {	
+	html += table_header;
+	html += '<tr>';
+	html += '<td>'+elem.name+'</td>';
+	html += '<td>'+elem.term+'</td>';
+	html += '<td>'+Math.round(elem.ICnuno*100)/100+'</td>';
+	html += '<td>'+Math.round(elem.ICzhou*100)/100+'</td>';
+	html += '<td>';
+	for (var i in elem.gene) {
+	    html += elem.gene[i];
+	    if (i != elem.gene.length - 1)
+		html += ", ";
 	}
-	
-	html += '</table>';
-		
-	element.innerHTML = html;
+	html += '</td>';
+	html += '</tr>';
+    }
+    html += '</table>';    
+    element.innerHTML = html;
 }
 	
-
 var treeInteractor = function (e) {
     var rect = canvas.getBoundingClientRect();  
     var pickX = e.clientX - rect.left;
-    var pickY = e.clientY - rect.top; //modifié
+    var pickY = e.clientY - rect.top;
     var m = window.fatum.pick(pickX, pickY);  
     if(!m) return;
-    changeSize(data, 1); // s * k = G / 2
+    changeSize(data, 1);
     recSum(data);
     changeColor(data, true);
     changeColor(markRev[m.id()], false);
@@ -237,8 +233,6 @@ var treeInteractor = function (e) {
     updateMarks(m);
     window.fatum.animate(500);
 }
-
-
 
 var initVis = function () {
   window.fatum = Fatum.createFatumContext(canvas);
